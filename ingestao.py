c229a5f28f2bf0344306e1225c78b36784f5642d
Vlad <vladjr83@gmail.com>
2026-07-02 11:35:59 -0300

    feat: adiciona script de importação

--- ingestao.py
+++ ingestao.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+clientes = pd.read_csv( "data/clientes_churn.csv" )
+clientes['data_cadastro'] = pd.to_datetime(clientes['data_cadastro'], errors='coerce', format='mixed')
+data_out = clientes['data_cadastro'].isna().sum()
+
+amostra = clientes.head(10)
+percent = (data_out / clientes.shape[0] ) * 100
+
+print(amostra)
+print(f"Arquivo importado e analisado: possui {clientes.shape[0]} linhas e {clientes.shape[1]} colunas")
+print(f"E em {data_out} linhas os dados de data estão inválidos, correspondendo à {percent:.2f}% das linhas - investigar contrato junto à source") 
+
+
